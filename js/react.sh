00d3225fb5e2bc76d8de0474ebaa057e88371586
Karim Alibhai <karim@alibhai.co>
2019-06-24 00:59:59 -0400

    Add: start & build scripts to react

--- js/react.sh
+++ js/react.sh
@@ -10,3 +10,9 @@ fetch_file "js/react/index.html" "src/index.html"
 fetch_file "js/react/app.jsx" "src/app.jsx"
 fetch_file "js/react/lib/axios.js" "src/lib/axios.js"
 fetch_file "js/react/views/home.jsx" "src/views/home.jsx"
+
+npm install --save-dev \
+	parcel-bundler
+
+jq '.scripts.start = .scripts.start // "parcel src/index.html"' package.json > package.new.json && mv package.new.json package.json
+jq '.scripts.build = .scripts.build // "parcel build src/index.html"' package.json > package.new.json && mv package.new.json package.json
